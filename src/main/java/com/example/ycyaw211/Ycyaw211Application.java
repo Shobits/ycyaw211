a31e9380fef57025b5d9ecb0dcd7a9a81f422126
Shobits <79295797@qq.com>
2022-08-27 20:18:38 +0800

    Initial commit

---
+++ src/main/java/com/example/ycyaw211/Ycyaw211Application.java
@@ -0,0 +1,13 @@
+package com.example.ycyaw211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ycyaw211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ycyaw211Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ycyaw211/Ycyaw211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ycyaw211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ycyaw211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
